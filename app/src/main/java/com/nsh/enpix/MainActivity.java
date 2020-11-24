f119dd866341795648da7757aec62ec70efa5931
ThisIsNsh <hadanis.singh@gmail.com>
2020-11-24 15:00:04 +0530

    New Release 1.2

--- app/src/main/java/com/nsh/enpix/MainActivity.java
+++ app/src/main/java/com/nsh/enpix/MainActivity.java
@@ -1,21 +1,22 @@
 package com.nsh.enpix;
 
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
-import android.widget.EditText;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.nsh.enpixview.EnpixView;
 
+import java.security.NoSuchAlgorithmException;
+
 public class MainActivity extends AppCompatActivity {
 
-    private EnpixView image1;
-    private EditText grain;
+    private EnpixView image1, image2;
 
-    String key = "FirstnameLastnameEmailID@Something.com";
+    String key = "firstname.lastname@email.com-nameofuser-mobilenumber";
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -23,10 +24,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        grain = findViewById(R.id.grain);
-
         image1 = findViewById(R.id.image1);
-        image1.setImage(BitmapFactory.decodeResource(this.getResources(), R.drawable.pic3), key, Integer.parseInt(grain.getText().toString()), 100000);
+        image2 = findViewById(R.id.image2);
+
+        try {
+            Bitmap im = image1.getEncryptImage(BitmapFactory.decodeResource(this.getResources(), R.drawable.pic3), key, 100000);
+            image1.setImageBitmap(im);
+            image2.decryptImage(im, "firstname.lastname@email.co-nameofuser-mobilenumber", 100000);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
 
     }
 
